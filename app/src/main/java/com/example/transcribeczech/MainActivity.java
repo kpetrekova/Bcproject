a332f03332262eef5979eb60b879fc2fd112f4a8
Tori <pk@example.com>
2023-12-28 17:47:11 +0100

    regressive assimilation inside word

--- app/src/main/java/com/example/transcribeczech/MainActivity.java
+++ app/src/main/java/com/example/transcribeczech/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.transcribeczech;
 
 
-import static android.text.TextUtils.indexOf;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,16 +9,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private EditText et;
-    private final String[] normalVocals = {"a","e","i","o","u","y","ě"};
-    private final String[] longVocals = {"á","é","í","ó","ú","ý"};
-    private final String[] ipaVocals = {"a","ɛ","ɪ","o","u","ɪ","jɛ"};
-    private final String[] toBePalatalConsonants = {"d","t","n"};
-    private final String[] ipaPalatalConsonants = {"ɟ","c","ɲ"};
-    private final String[] normalConsonantPairs = {"p","b","t","d","ť","ď","k","g","c","dz","č","dž","ř2","ř","f","v","s","z","š","ž","h2","h"};
-    private final String[] ipaConsonantPairs = {"p","b","t","d","c","ɟ","k","g","t͜s","d͜z","t͜ʃ","d͜ʒ","r̝̊","r̝","f","v","s","z","ʃ","ʒ","x","ɦ"};
+    private final String[] normalVocals = {"a", "e", "i", "o", "u", "y", "ě"};
+    private final String[] longVocals = {"á", "é", "í", "ó", "ú", "ý"};
+    private final String[] ipaVocals = {"a", "ɛ", "ɪ", "o", "u", "ɪ", "jɛ"};
+    private final String[] toBePalatalConsonants = {"d", "t", "n"};
+    private final String[] ipaPalatalConsonants = {"ɟ", "c", "ɲ"};
+    private final String[] normalConsonantPairs = {"p", "b", "t", "d", "ť", "ď", "k", "g", "c", "č", "ř", "f", "v", "s", "z", "š", "ž", "h", "w"};
+    private final String[] ipaConsonantPairs = {"p", "b", "t", "d", "c", "ɟ", "k", "g", "t͜s", "t͜ʃ", "r̝", "f", "v", "s", "z", "ʃ", "ʒ", "ɦ", "v"};
+
+    enum CharType {VOICED, VOICELESS, VOCAL, OTHER}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,16 +39,18 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(origText + "\n\n" + transcribedText);
         et.setText("");
     }
+
     public String transcribe(String origText) {
 
         StringBuilder transcribedText = new StringBuilder();
 
-        for (String word : origText.split(" ") ) {
+        for (String word : origText.split(" ")) {
 
             word += " ";
             String[] text = word.split("");
 
             for (int i = 0; i < text.length; i++) {
+
                 // vocals
                 if (text[i].matches("[aeiouyě]")) {
                     transcribedText.append(ipaVocals[getIndexOfString(normalVocals, text[i])]);
@@ -62,13 +67,13 @@ public class MainActivity extends AppCompatActivity {
                 // ch
                 else if (text[i].equals("c") && text[i + 1].equals("h")) {
                     transcribedText.append("x");
-                    i++;
+                    i++;    // skip "h" in original text
                 }
 
                 // palatals
                 else if (text[i].equals("m") && text[i + 1].matches("ě")) {
                     transcribedText.append("mɲɛ");
-                    i++;
+                    i++; // skip "ě" after palatals, so it won't become "jɛ"
                 } else if (text[i].matches("[dtn]") && text[i + 1].matches("[iíě]")) { // ".*nismus" is for now palatal
                     if (i < 5 && word.matches("od.*|před.*|nad.*|pod.*|před.*|post.*|ad.*|red.*|in.*|en.*") && !word.matches("post[ií][hžt].*|postí|odiv.*|přediv.*|nadi[tv].*|podiv.*")) { // i < 5 because in first string array is "", and some prefixes have 4 characters
                         transcribedText.append(ipaConsonantPairs[getIndexOfString(normalConsonantPairs, text[i])]);
@@ -78,19 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
                     if (text[i + 1].equals("ě")) {
                         transcribedText.append("ɛ");
-                        i++;
-                    }
-                }
-
-                // pair consonants
-                else if (text[i].matches("[pbtdťďkgcčřfvszšžh]|dz|dž")) {
-                    boolean voiced = getIndexOfString(normalConsonantPairs, text[i]) % 2 == 1;
-
-                    // voiceless end of word
-                    if (text[i + 1].equals(" ") && voiced) {          // pozor na zápis dat z OCR
-                        transcribedText.append(ipaConsonantPairs[getIndexOfString(normalConsonantPairs, text[i]) - 1]);
-                    } else {
-                        transcribedText.append(ipaConsonantPairs[getIndexOfString(normalConsonantPairs, text[i])]);
+                        i++; // skip "ě" after palatals, so it won't become "jɛ"
                     }
                 }
 
@@ -102,17 +95,20 @@ public class MainActivity extends AppCompatActivity {
                         transcribedText.append("ɱ");
                     }
 
+                } else if (text[i].matches("[pbtdťďkgcčřfvszšžh]")) {
+                    transcribedText.append(ipaConsonantPairs[getIndexOfString(normalConsonantPairs, text[i])]);
                 } else {
                     transcribedText.append(text[i]);
                 }
             }
         }
-        return transcribedText.toString();
+        return regressiveAssimilation(transcribedText.toString());
     }
-    public int getIndexOfString(String[] list, String match){
+
+    public int getIndexOfString(String[] list, String match) {
         int index = -1;
-        for (int i = 0; i < list.length; i++){
-            if (match.equals(list[i])){
+        for (int i = 0; i < list.length; i++) {
+            if (match.equals(list[i])) {
                 index = i;
                 break;
             }
@@ -120,55 +116,49 @@ public class MainActivity extends AppCompatActivity {
         return index;
     }
 
-
-/*
-    public String regressiveAssimilation(String transcribedText){
-        String[] ipaVoicedConsonant = {"b","d","ɟ","g","r̝","v","z","ʒ","ɦ","m","n","r","j","l","ŋ","ɱ","ɲ"};
-        String[] ipaVoicelessConsonant = {"p","t","c","k","r̝̊","f","s","ʃ","x"};
-
-
+    public String regressiveAssimilation(String transcribedText) {
+        String[] ipaVoicedConsonant = {"b", "d", "ɟ", "g", "r̝", "v", "z", "ʒ", "ɦ", "m", "n", "r", "j", "l", "ŋ", "ɱ", "ɲ"};
+        String[] ipaVoicelessConsonant = {"p", "t", "c", "k", "r̝̊", "f", "s", "ʃ", "x"};
+        String[] words = transcribedText.split(" ");
 
         String[] text = transcribedText.split("");
-        StringBuilder assimilatedText = new StringBuilder();
-        String typeI = " "; // text[i] -- voiced, voiceless, vocal
-        String typeII = " "; // text [i+1] -- voiced, voiceless, vocal
 
-        for (int i = text.length; i > 0; i--){
-            typeI = consonantType(text[i]);
-            typeII = consonantType(text[i+1]);
-            if (typeI.equals(typeII)){
-                assimilatedText.append(text[i]);
-            }
-            else if (typeI.equals("voiced") && typeII.equals("voiceless")){
-                assimilatedText.append(ipaVoicelessConsonant[getIndexOfString(ipaVoicedConsonant, text[i])]);
-            }
-            else if (typeI.equals("voiceless") && typeII.equals("voiced")){
-                assimilatedText.append(ipaVoicedConsonant[getIndexOfString(ipaVoicelessConsonant, text[i])]);
-            }
-            // if (text[i].equals(":")) podívej se ještě o jedno vedle
-            else{
-                assimilatedText.append(text[i]);
-            }
+        for (int i = text.length-2; i > -1 ; i--){
+            CharType charI = getCharacterType(text[i]);
+            CharType charII = getCharacterType(text[i+1]);
 
+            if (charI.equals(CharType.VOICELESS) && charII.equals(CharType.VOICED) && !text[i].matches("[xŋɱɲjlrmn ]")){  // regex for unpaired consonants and space
+                text[i] = ipaVoicedConsonant[getIndexOfString(ipaVoicelessConsonant, text[i])];
+            } else if (charI.equals(CharType.VOICED) && charII.equals(CharType.VOICELESS) && !text[i].matches("[xŋɱɲjlrmn ]")) { // regex for unpaired consonants and space
+                text[i] = ipaVoicelessConsonant[getIndexOfString(ipaVoicedConsonant, text[i])];
+            } else {
+                continue;
+            }
         }
-
-        return assimilatedText.reverse().toString();
+        return arrayToString(text);
     }
 
-    public String consonantType(String ch) {
-        if (ch.matches("[bdɟgr̝vzʒɦmnrjlŋɱɲ]")){
-            return "voiced";
+
+
+
+    public CharType getCharacterType(String ch){
+        if (ch.matches("[bdɟgvzʒɦ]")) {
+            return CharType.VOICED;
+        } else if (ch.matches("[ptckfsʃx ]")) {
+            return CharType.VOICELESS;
+        } else if (ch.matches("[aɛɪoui:]")) {   //asi zbytečné
+            return CharType.VOCAL;
+        } else {
+            return CharType.OTHER;
         }
-        else if (ch.matches("[ptckr̝̊fsʃx]")){
-            return "voiceless";
-        }
-        else if (ch.matches("[aɛɪoui]")){
-            return "vocal";
-        }
-        else if (ch.matches(":")){
-            return ":";
-        }
-        return "_";
     }
-*/
+
+    public String arrayToString(String[] list){
+        StringBuilder result = new StringBuilder();
+        for (String s : list) {
+            result.append(s);
+        }
+        return result.toString();
+    }
 }
+
