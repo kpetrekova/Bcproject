fcd27689b42c068c42d5055b8d88b0e756122341
Tori <pk@example.com>
2024-03-21 22:15:06 +0100

    no more breaking on "eni.*" dcera, srdce, denní -i.* suffixes in contact with dtn mv, mf q, w, x

--- app/src/main/java/com/example/transcribeczech/MainActivity.java
+++ app/src/main/java/com/example/transcribeczech/MainActivity.java
@@ -34,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private final String[] ipaVocals = {"a", "ɛ", "ɪ", "o", "u", "ɪ", "jɛ"};
     private final String[] toBePalatalConsonants = {"d", "t", "n"};
     private final String[] ipaPalatalConsonants = {"ɟ", "c", "ɲ"};
-    private final String[] normalConsonantPairs = {"p", "b", "t", "d", "ť", "ď", "k", "g", "c", "č", "ř", "f", "v", "s", "z", "š", "ž", "h", "w"};
-    private final String[] ipaConsonantPairs = {"p", "b", "t", "d", "c", "ɟ", "k", "g", "t͜s", "t͜ʃ", "r̝", "f", "v", "s", "z", "ʃ", "ʒ", "ɦ", "v"};
+    private final String[] normalConsonantPairs = {"p", "b", "t", "d", "ť", "ď", "k", "g", "c", "č", "ř", "f", "v", "s", "z", "š", "ž", "h", "w","q","x","n"};
+    private final String[] ipaConsonantPairs = {"p", "b", "t", "d", "c", "ɟ", "k", "g", "t͜s", "t͜ʃ", "r̝", "f", "v", "s", "z", "ʃ", "ʒ", "ɦ", "v","kv","ks","n"};
 
     enum CharType {VOICED, VOICELESS, VOCAL, OTHER}
 
@@ -63,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public String getTextFromImage(Bitmap bitmap){
-       // Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.cestina);
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
         if (!textRecognizer.isOperational()){
             Toast.makeText(getApplicationContext(), "No text", Toast.LENGTH_SHORT).show();
@@ -149,12 +148,26 @@ public class MainActivity extends AppCompatActivity {
                     i++;    // skip "h" in original text
                 }
 
+                // dcera, srdce
+                else if (text[i].equals("d") && word.matches("dcer.*|srdc.*")){
+                    transcribedText.append("t͜s");
+                    i++;   // skip "c" in original text
+                }
+                //x-denní
+                else if (text[i].equals("n") && word.matches(".*denn.*")){
+                    transcribedText.append("n");
+                    i++;    // skip second "n" in original text
+                }
+
                 // palatals
                 else if (text[i].equals("m") && text[i + 1].matches("ě")) {
                     transcribedText.append("mɲɛ");
                     i++; // skip "ě" after palatals, so it won't become "jɛ"
-                } else if (text[i].matches("[dtn]") && text[i + 1].matches("[iíě]")) { // ".*nismus" is for now palatal
+                }
+                else if (text[i].matches("[dtn]") && text[i + 1].matches("[iíě]")) { // "technika" etc is for now palatal
                     if (i < 5 && word.matches("od.*|nad.*|pod.*|před.*|post.*|ad.*|red.*|in.*|en.*") && !word.matches("post[ií][hžt].*|postí|odiv.*|přediv.*|nadi[tv].*|podiv.*")) { // i < 5 because in first string array is "", and some prefixes have 4 characters
+                        transcribedText.append(ipaConsonantPairs[getIndexOfString(normalConsonantPairs, text[i])]);
+                    } else if (word.matches(".*ing.*|.*iont.*|.*itid.*|.*itud.*|.*iád.*|.*ián.*|.*iatr.* |.*istik.*|.*[^vo][dtn]ikac.*|.*[dtn]iz[ou].*|.*[dtn]i[sz]m.*")&& !word.matches("nizozem.*|proti.*")){
                         transcribedText.append(ipaConsonantPairs[getIndexOfString(normalConsonantPairs, text[i])]);
                     } else {
                         transcribedText.append(ipaPalatalConsonants[getIndexOfString(toBePalatalConsonants, text[i])]);
@@ -166,20 +179,21 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                // articulatory assimilation of mn around kg
-                else if (text[i].matches("[mn]") && (text[i + 1].matches("[kg]"))) {
-                    if (text[i].equals("n") && text[i + 1].matches("[kg]")) {
-                        transcribedText.append("ŋ");
-                    } else {
-                        transcribedText.append("ɱ");
-                    }
+                // articulatory assimilation of n around kg
+                else if (text[i].matches("n") && (text[i + 1].matches("[kg]"))) {
+                    transcribedText.append("ŋ");
+                }
+                // articulatory assimilation of m before vf
+                else if (text[i].matches("m") && (text[i + 1].matches("[kg]"))) {
+                            transcribedText.append("ɱ");
 
-                } else if (text[i].matches("[pbtdťďkgcčřfvszšžh]")) {
+                } else if (text[i].matches("[pbtdťďkgcčřfvszšžhwqx]")) {
                     transcribedText.append(ipaConsonantPairs[getIndexOfString(normalConsonantPairs, text[i])]);
                 } else {
                     transcribedText.append(text[i]);
                 }
             }
+
         }
         return regressiveAssimilation(transcribedText.toString());
     }
@@ -220,7 +234,7 @@ public class MainActivity extends AppCompatActivity {
                 charII = getCharacterType(text[i+1]);
             }
 
-            if (charI.equals(CharType.VOICELESS) && charII.equals(CharType.VOICED) && !text[i].matches("[x ]")) {  // ch and space can't become voiced
+            if (charI.equals(CharType.VOICELESS) && charII.equals(CharType.VOICED) && !text[i].matches("[x ]") && !text[i+1].matches("v")) {  // ch and space can't become voiced, v doesn't make consonants voiced
                 text[i] = ipaVoicedConsonant[getIndexOfString(ipaVoicelessConsonant, text[i])];
             } else if (prep && charI.equals(CharType.VOICED) && charII.equals(CharType.VOCAL)){ // one-syllable voiced preposition reaction to word starting with vocal
                 text[i] = ipaVoicelessConsonant[getIndexOfString(ipaVoicedConsonant, text[i])];
